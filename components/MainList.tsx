cd71f9ece97b2bb177b6771cd9da5e48b08b4d67
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-27 22:07:19 +0200

    Card added

--- components/MainList.tsx
+++ components/MainList.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Image from 'next/image'
+import python from '../public/react.jpg'
+
 
 const MainList = () => {
   return (
-    <div>MainList</div>
+    <section className='bg-white max-container padding-container'>
+        <div className='grid grid-cols-1 py-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 pt-4 lg:pt-8'>
+            <div className='flex flex-col gap-3'>
+                <Image src={python} alt='image' className='w-full h-[200px] object-cover' />
+                <h1 className='text-[#FF3B1D] font-dancing-700 text-2xl'>DevSpace</h1>
+                <p className='text-[#191919] font-robot-300 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.</p>
+            </div>
+            <div className='flex flex-col gap-3'>
+                <Image src={python} alt='image' className='w-full h-[200px] object-cover' />
+                <h1 className='text-[#FF3B1D] font-dancing-700 text-2xl'>DevSpace</h1>
+                <p className='text-[#191919] font-robot-300 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.</p>
+            </div>
+            <div className='flex flex-col gap-3'>
+                <Image src={python} alt='image' className='w-full h-[200px] object-cover' />
+                <h1 className='text-[#FF3B1D] font-dancing-700 text-2xl'>DevSpace</h1>
+                <p className='text-[#191919] font-robot-300 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.</p>
+            </div>
+            <div className='flex flex-col gap-3'>
+                <Image src={python} alt='image' className='w-full h-[200px] object-cover' />
+                <h1 className='text-[#FF3B1D] font-dancing-700 text-2xl'>DevSpace</h1>
+                <p className='text-[#191919] font-robot-300 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.</p>
+            </div>
+        </div>
+    </section>
   )
 }
 
