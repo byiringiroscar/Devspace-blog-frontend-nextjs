98a19dde4cb9e1ba79a50ea35c0a90a60a9abc8a
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-27 22:37:49 +0200

    List blog dynamically

--- components/CardBlog.tsx
+++ components/CardBlog.tsx
@@ -2,12 +2,33 @@ import React from 'react'
 import Image from 'next/image'
 import python from '../public/react.jpg'
 
-const CardBlog = () => {
+
+type BlogType = {
+    id: string,
+    title: string,
+    blog_image: string,
+    body: string,
+    created_at: string,
+}
+
+
+
+const CardBlog = ({
+    id,
+    title,
+    blog_image,
+    body,
+    created_at,
+}: BlogType) => {
   return (
     <div className='flex flex-col gap-3 bg-white p-2 rounded'>
-                <Image src={python} alt='image' className='w-full h-[200px] object-cover rounded' />
-                <h1 className='text-[#FF3B1D] font-dancing-700 text-2xl'>DevSpace</h1>
-                <p className='text-[#191919] font-robot-300 text-[14px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.</p>
+                <Image src={blog_image} width={50} height={50} alt='image' className='w-full h-[200px] object-cover rounded' />
+                <h1 className='text-[#FF3B1D] font-dancing-700 text-2xl'>{title}</h1>
+                <p className='text-[#191919] font-robot-300 text-[14px]'>{body}</p>
+                <div className='flex justify-between items-center'>
+                    <p className='text-[#191919] font-robot-300 text-[14px]'>{created_at}</p>
+                    <button className='bg-[#FF3B1D] text-white rounded-md px-3 py-1 font-robot-500 text-[12px]'>Read</button>
+                    </div>
             </div>
   )
 }
--- components/MainList.tsx
+++ components/MainList.tsx
@@ -1,37 +1,54 @@
+'use client'
 import React from 'react'
 import CardBlog from './CardBlog'
+import useSWR from 'swr'
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
+
+
+const fetcher = async(id: any) => {
+    const res = await fetch(`http://127.0.0.1:8000/blog`)
+    const data = await res.json()
+    return data
+  }
 
 
 
 const MainList = () => {
+    const { data, error, mutate, isLoading }  = useSWR('home', fetcher)	
+    if (isLoading) {
+        return (
+            <div className='max-container padding-container'>
+                    <div className='grid grid-cols-1 py-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 pt-4 lg:pt-8'>
+                    {/* Render loading skeleton */}
+                    {[...Array(16)].map((_, index) => (
+                        <div key={index} className='p-6 bg-gray-300 rounded-md'>
+                        <Skeleton height={150} />
+                        <Skeleton height={20} width={120} />
+                        <Skeleton height={15} width={80} />
+                        </div>
+                    ))}
+                    </div>
+                </div>
+        )
+    }
+    if (error) {
+        return (
+          <div className='max-container padding-container'>
+            <div className='grid grid-cols-1 py-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 pt-4 lg:pt-8'>
+              <div className='error-card'>
+                <h3 className='text-center'>There was an error fetching the data</h3>
+              </div>
+            </div>
+          </div>
+        );
+      }
   return (
     <section className='bg-[#F2F2F2] max-container padding-container'>
         <div className='grid grid-cols-1 py-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 pt-4 lg:pt-8'>
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
+        {data.map((item: any) => (
+                <CardBlog key={item.id} {...item} />
+            ))}
         </div>
     </section>
   )
