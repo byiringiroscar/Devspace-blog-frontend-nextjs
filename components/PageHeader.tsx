d1d01efb82f6715f0cd7aa94fe37c664d3b022b7
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-28 00:26:09 +0200

    Add shadcn

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -5,7 +5,7 @@ const PageHeader = () => {
     <div className='w-full pt-10 pb-12 bg-[#191919] flex flex-col gap-3 items-center'>
         <h1 className='text-white font-dancing-700 text-4xl'>Welcome to our Blogs</h1>
         <p className='text-white font-robot-300 text-[14px]'>We build amazing blogs</p>
-        <button className='bg-[#FF3B1D] text-white rounded-md px-3 py-1 font-robot-500 text-[12px]'>Let&apos;s talk</button>
+        <button className='bg-[#FF3B1D] text-white rounded-md px-3 py-1 font-robot-500 text-[12px]'>Add blog</button>
     </div>
   )
 }
