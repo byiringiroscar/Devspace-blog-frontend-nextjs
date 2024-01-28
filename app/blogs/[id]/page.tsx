85083c9f5d3dc286ebf20e1042587897d22280fe
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-28 14:27:59 +0200

    Update page.tsx

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -21,7 +21,7 @@ const BlogDetail = () => {
   return (
     <>
       <div className='w-full pt-10 pb-12 bg-[#191919] flex flex-col gap-3 items-center'>
-          <h1 className='text-white font-dancing-700 text-lg lg:text-[25px]'>Blog Details</h1>
+          <h1 className='text-[#FF3B1D] font-dancing-700 text-lg lg:text-[25px]'>Blog Details</h1>
           <p className='text-white font-robot-300 text-[14px]'>We build amazing blogs</p>
       </div>
       <div className='max-container padding-container'>
