16a7f5b79584a22f8c095b8efbe948e72bbf365d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-28 15:05:10 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['127.0.0.1', 'images.pexels.com', 'devspace-60v7.onrender.com'],
+        // domains: ['127.0.0.1', 'images.pexels.com', 'devspace-60v7.onrender.com'],
+        remotePatterns: [
+            {
+              protocol: 'http', // Adjust for HTTPS if needed
+              hostname: '127.0.0.1',
+            },
+            {
+              protocol: 'https',
+              hostname: 'images.pexels.com',
+            },
+            {
+              protocol: 'https',
+              hostname: 'devspace-60v7.onrender.com',
+              pathname: '**', // Matches any pathname on this hostname
+            },
+          ],
     },
 };
 
